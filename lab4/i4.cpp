d4279e6755cb041828de7842de628fe1d27bad04
RealMagnolia <qaraoygaberilme@gmail.com>
2025-10-03 08:57:37 +0500

    Update i4.cpp

--- lab4/i4.cpp
+++ lab4/i4.cpp
@@ -2,81 +2,107 @@
 using namespace std;
 
 struct Node {
-	int data;
-	Node* left;
-	Node* right;
+    int data;
+    int cnt;
+    Node* left;
+    Node* right;
 
-	Node(int data) {
-		this->data = data;
-		left = NULL;
-		right = NULL;
-	} 
+    Node(int data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        this->cnt = 1;
+    } 
 };
 
 Node* getMin(Node* root) {
-	while (root->left != NULL) {
-		root = root->left;
-	}
-	return root;
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
 }
 
-Node* add(Node* root, int data) {
-	if (root == NULL) {
-		return new Node(data);
-	} else if (root->data > data) {
-		if (root->left == NULL) {
-			root->left = new Node(data);
-		} else {
-			root->left = add(root->left, data);
-		}
-	} else if (root->data < data) {
-		if (root->right == NULL) {
-			root->right = new Node(data);
-		} else {
-			root->right = add(root->right, data);
-		}
-	}
-	return root;
+Node* insert(Node* root, int data) {
+    if (root == NULL) {
+        return new Node(data);
+    }
+    if (root->data == data) {
+        root->cnt++;
+        return root;
+    }
+    if (root->data > data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
 }
 
 Node* deleteNode(Node* root, int data) {
-	if (root == NULL) {
-		return NULL;
-	} else if (root->data > data) {
-		root->left = deleteNode(root->left, data);
-		return root;
-	} else if (root->data < data) {
-		root->right = deleteNode(root->right, data);
-		return root;
-	} else {
-		if (root->left == NULL && root->right == NULL) {
-			delete root;
-			return NULL;
-		} else if (root->right == NULL) {
-			Node* temp = root->left;
-			delete root;
-			return temp;
-		} else if (root->left == NULL) {
-			Node* temp = root->right;
-			delete root;
-			return temp;
-		} else {
-			Node* temp = getMin(root->right);
-			root->data = temp->data;
-			root->right = deleteNode(root->right, temp->data);
-			return root;
-		}
-	}
+    if (root == NULL) {
+        return NULL;
+    }
+    if (root->data > data) {
+        root->left = deleteNode(root->left, data);
+        return root;
+    }
+    if (root->data < data) {
+        root->right = deleteNode(root->right, data);
+        return root;
+    }
+    if (root->cnt > 1) {
+        root->cnt--;
+        return root;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        delete root;
+        return NULL;
+    }
+    if (root->right == NULL) {
+        Node* temp = root->left;
+        delete root;
+        return temp;
+    }
+    if (root->left == NULL) {
+        Node* temp = root->right;
+        delete root;
+        return temp;
+    }
+    Node* temp = getMin(root->right);
+    root->data = temp->data;
+    root->cnt = temp->cnt;
+    root->right = deleteNode(root->right, temp->data);
+    return root;
 }
 
-Node* find(Node* root, int x) {
-	if (root == NULL) {
-		return NULL;
-	} else if (root->data == x) {
-		return root;
-	} else if (root->data < x) {
-		return find(root->right, x);
-	} else {
-		return find(root->left, x);
-	}
+int cnt(Node* root, int x) {
+    if (root == NULL) {
+        return 0;
+    }
+    if (root->data == x) {
+        return root->cnt;
+    }
+    if (root->data > x) {
+        return cnt(root->left, x);
+    }
+    return cnt(root->right, x);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Node* root = NULL;
+    for (int i = 0; i < n; i++) {
+        string query;
+        int data;
+        cin >> query >> data;
+        if (query == "insert") {
+            root = insert(root, data);
+        } else if (query == "cnt") {
+            cout << cnt(root, data) << endl;
+        } else if (query == "delete") {
+            root = deleteNode(root, data);
+        }
+    }
+    return 0;
 }
